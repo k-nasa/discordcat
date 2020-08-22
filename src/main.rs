9cab4ec7d0a8b4f83a19517dcb72022054b83e2c
k-nasa <htilcs1115@gmail.com>
2020-08-22 20:03:18 +0900

    Add cli base + configure option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
+
+use anyhow::Result;
+
+use std::io;
+use std::io::Write;
+use std::process::exit;
+
+const CFG_FLAG: &str = "configure";
+
+fn main() -> Result<()> {
+    let app = build_app();
+
+    let matches = app.get_matches();
+
+    if matches.is_present(CFG_FLAG) {
+        configure_discord_webhook();
+        exit(0)
+    }
+
+    Ok(())
+}
+
+fn configure_discord_webhook() -> Result<()> {}
+
+fn build_app() -> App<'static, 'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .setting(AppSettings::DeriveDisplayOrder)
+        .setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name(CFG_FLAG)
+                .long("configure")
+                .short("c")
+                .help("TODO")
+                .takes_value(false),
+        )
 }
